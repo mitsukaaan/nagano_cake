978ad0010509ee284c03118ef746c06bc6a17bf7
mitsuka1223star@gmail.com <ec2-user@ip-172-31-92-246.ec2.internal>
2022-04-02 05:25:34 +0000

    customer sign up f

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,33 +1,34 @@
 class Admin::ItemsController < ApplicationController
-    
+
     def new
         @item =Item.new
-        
-    end 
-    
-    def index 
+        @genre =Genre.all
+
+    end
+
+    def index
         @items = Item.all
-    end 
-    
+    end
+
     def create
         item = Item.new(item_params)
         item.save!
         redirect_to '/admin/items'
-    end 
-    
+    end
+
     def show
-    end 
-    
+    end
+
     def edit
-    end 
-    
+    end
+
     def update
-    end 
-    
+    end
+
     private
-    
+
     def item_params
         params.require(:item).permit(:name,:introduction,:genre_id,:price,:is_active,:image)
-    end 
-    
+    end
+
 end
